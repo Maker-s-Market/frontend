74bba4b99bf711dfab9f2d7d4b6315031dbd4baa
v1centebarros <vmabarros@ua.pt>
2023-10-16 16:05:39 +0100

    Home Component with Hero, TopCategories and Highlights components

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,14 +1,12 @@
-import {useQuery} from "react-query";
-import fetchTodos from "../../api/fetchTodos";
+import {TopCategories} from "../../components/home/topCategories/index.js";
+import {Hero} from "../../components/home/hero/index.js";
+import {Highlights} from "../../components/home/highlights/index.js";
 
 export const Home = (props) => {
-    const {data, error, isLoading} = useQuery("todos", fetchTodos)
     return <div>
-        <div className={"text-4xl text-white font-bold"}>Homepage</div>
-        {isLoading && <div>Loading...</div>}
-        {error && <div>Error: {error}</div>}
-        {data && <div>{JSON.stringify(data)}</div>}
+        <Hero/>
+        <TopCategories/>
+        <Highlights/>
     </div>
-
 
 };
