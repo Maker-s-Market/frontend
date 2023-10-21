5ebe2af478c7905dc8ff1f5e2aae2de3d3db5d79
v1centebarros <vmabarros@ua.pt>
2023-10-21 16:08:25 +0100

    Add Link to the category on Top Categories

--- src/components/home/topCategories/topCategories.jsx
+++ src/components/home/topCategories/topCategories.jsx
@@ -2,28 +2,30 @@ import {fetchTopCategories} from "../../../api/fetchCategories.js";
 import {useQuery} from "react-query";
 import {Loading} from "../../common/loading/index.js";
 import {ErrorMessage} from "../../common/error/index.js";
+import {Link} from "react-router-dom";
 
 export const TopCategories = (props) => {
 
 
-        const {data: topCategories, error, isLoading, isSuccess} = useQuery("categories", fetchTopCategories)
+    const {data: topCategories, error, isLoading, isSuccess} = useQuery("categories", fetchTopCategories)
 
-        return <>
-            <h1 className={"text-5xl font-bold text-center py-12"}>Top Categories</h1>
-            {isLoading && <Loading/>}
-            <div className={"grid grid-cols-2  lg:grid-cols-4 place-items-center gap-y-4 md:mx-auto md:w-3/4"}>
-                {isSuccess && topCategories.map((item) => {
-                    return (<div key={item.id} className={"w-full md:w-48 flex flex-col place-items-center space-y-4"}>
+    return <>
+        <h1 className={"text-5xl font-bold text-center py-12"}>Top Categories</h1>
+        {isLoading && <Loading/>}
+        <div className={"grid grid-cols-2  lg:grid-cols-4 place-items-center gap-y-4 md:mx-auto md:w-3/4"}>
+            {isSuccess && topCategories.map((item) => {
+                return (<Link to={`/category/${item.id}`} key={item.id}>
+                    <div key={item.id} className={"w-full md:w-48 flex flex-col place-items-center space-y-4"}>
                         <div className="avatar">
                             <div className="w-40 rounded-full">
                                 <img src={"https://picsum.photos/200"} alt={"Temp"}/>
                             </div>
                         </div>
                         <p className={"text-3xl font-semibold"}>{item.name}</p>
-                    </div>)
-                })}
-            </div>
-            {error && <ErrorMessage/>}
-        </>
-    }
-;
+                    </div>
+                </Link>)
+            })}
+        </div>
+        {error && <ErrorMessage/>}
+    </>
+};
