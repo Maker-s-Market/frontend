8b6e20503bae87800b13b4623452fc205bf0d5d7
v1centebarros <vmabarros@ua.pt>
2023-10-22 13:52:16 +0100

    MM-36 Add Product icon on sidebar

--- src/components/sidebar/menu/menu.jsx
+++ src/components/sidebar/menu/menu.jsx
@@ -1,5 +1,5 @@
 import {useAuth} from "../../../hooks/useAuth.js";
-import {BsFillChatFill, BsSearch} from "react-icons/bs";
+import {BsFillChatFill, BsFillPlusCircleFill, BsSearch} from "react-icons/bs";
 import {IoMdSettings} from "react-icons/io";
 import {BiSolidExit} from "react-icons/bi";
 import {Link} from "react-router-dom";
@@ -9,15 +9,15 @@ export const Menu = (props) => {
     const {isLogged} = useAuth();
     return <>
         <li className="font-bold"><Link to={"/search"}><BsSearch/>Search</Link></li>
-        {!isLogged() && <>
-            <li className="font-bold"><a>Sign In</a></li>
-        </>}
-        {isLogged() && <>
+        {isLogged() ? <>
+            <li className={"font-bold"}><Link to={"/product/add"}><BsFillPlusCircleFill/>Anunciar Produto</Link></li>
             <li className="font-bold"><a><BsFillChatFill/>Chat<CommingSoon/></a></li>
 
             <li className="font-bold"><a><IoMdSettings/>Definições<CommingSoon/></a></li>
 
             <li className="font-bold"><a><BiSolidExit/>Sign Out</a></li>
+        </> : <>
+            <li className="font-bold"><a>Sign In</a></li>
         </>}
     </>
 };
