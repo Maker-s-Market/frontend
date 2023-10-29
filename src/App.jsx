45f73cabc502aef2a590eb0e597130139930e96c
Mateus Almeida <mbcsalmeida@gmail.com>
2023-10-29 21:53:46 +0000

    Icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import Router from './utils/Router'
 import {QueryClient, QueryClientProvider} from "react-query";
 import {AuthProvider} from "./contexts/auth.jsx";
 import {NotificationProvider} from "./contexts/notification.jsx";
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab,fas);
 
 function App() {
     const queryClient = new QueryClient()
