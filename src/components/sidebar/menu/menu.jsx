efcfe995878e6b5b99c8d6b5c3ce3d1cb1a81108
v1centebarros <vmabarros@ua.pt>
2023-11-04 15:35:53 +0000

    update sidebar profile and menu to be dynamic when authenticated

--- src/components/sidebar/menu/menu.jsx
+++ src/components/sidebar/menu/menu.jsx
@@ -1,12 +1,12 @@
-import {useAuth} from "../../../hooks/useAuth.js";
 import {BsFillChatFill, BsFillPlusCircleFill, BsSearch} from "react-icons/bs";
 import {IoMdSettings} from "react-icons/io";
 import {BiSolidExit} from "react-icons/bi";
 import {Link} from "react-router-dom";
 import {CommingSoon} from "../../common/commingSoon/index.js";
+import {useAuthContext} from "../../../contexts/auth.jsx";
 
 export const Menu = (props) => {
-    const {isLogged} = useAuth();
+    const {isLogged, logout} = useAuthContext();
     return <>
         <li className="font-bold"><Link to={"/search"}><BsSearch/>Search</Link></li>
         {isLogged() ? <>
@@ -15,9 +15,8 @@ export const Menu = (props) => {
 
             <li className="font-bold"><a><IoMdSettings/>Settings<CommingSoon/></a></li>
 
-            <li className="font-bold"><a><BiSolidExit/>Sign Out</a></li>
-        </> : <>
-            <li className="font-bold"><a>Sign In</a></li>
-        </>}
+            <li className="font-bold"><a onClick={() => logout()}><BiSolidExit/>Sign Out</a></li>
+        </> : <li className="font-bold"><Link to={"/signin"}>Sign In</Link></li>
+        }
     </>
 };
--- src/components/sidebar/profile/profile.jsx
+++ src/components/sidebar/profile/profile.jsx
@@ -1,18 +1,18 @@
-import {useAuth} from "../../../hooks/useAuth.js";
+import {useAuthContext} from "../../../contexts/auth.jsx";
 
 export const Profile = (props) => {
-  const {isAuthenticated} = useAuth();
+  const {isLogged,user} = useAuthContext();
   return <div>
 
-    {isAuthenticated && <div className="p-3 flex space-x-4">
+    {isLogged() && <div className="p-3 flex space-x-4">
       <div className="avatar ">
         <div className="w-24 rounded-full ">
-          <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="profile picture" />
+          <img src={user.photo || "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"} alt="profile picture" />
         </div>
       </div>
       <div className="flex flex-col justify-center">
-        <h1 className="text-lg">Name LastName</h1>
-        <h3 className="text-md">@username</h3>
+        <h1 className="text-lg">{user.name}</h1>
+        <h3 className="text-md">@{user.username}</h3>
         </div>
     </div>
     }
