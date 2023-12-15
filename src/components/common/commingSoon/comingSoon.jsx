529525413fe8a42433c4b86f327dde6ff987edc2
v1centebarros <vmabarros@ua.pt>
2023-12-15 12:14:18 +0000

    Fix component naming

--- src/components/common/commingSoon/comingSoon.jsx
+++ src/components/common/commingSoon/comingSoon.jsx
@@ -5,6 +5,6 @@
  * @param {object} props - The properties passed to the component.
  * @returns {JSX.Element} A div element with the text "Coming Soon".
  */
-export const CommingSoon = (props) => {
+export const ComingSoon = (props) => {
   return <div className="badge badge-secondary mx-2">Coming Soon</div>;
 };
